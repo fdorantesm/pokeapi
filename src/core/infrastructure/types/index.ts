db4e3deade0dbd0b12b08be7fe0814fb3496e5e5
Fernando Dorantes <fdorantesm@gmail.com>
2024-01-10 05:01:09 -0600

    feat: add create pokemon endpoint

--- src/core/infrastructure/types/index.ts
+++ src/core/infrastructure/types/index.ts
@@ -1,3 +1,3 @@
 export * from '@/core/infrastructure/types/database/database-connnection.type';
-export * from '@/core/infrastructure/types/general/json.type';
 export * from '@/core/infrastructure/types/http/http-server.type';
+export * from '@/core/types/general/json.type';
